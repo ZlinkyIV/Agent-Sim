dde4dbbb6ffaee9534d6b4d47f8ad3148ee8481b
Kaydon Sloan <zlinkyiv@gmail.com>
2023-12-09 17:52:43 -0600

    Reorganized module structure and misc cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-pub mod simulation {
+pub mod prelude {
+    pub use crate::state::*;
+    pub use crate::object::*;
+}
+
+
+pub mod state {
+    use crate::object::Object;
+
     pub struct SimulationManager<'a> {
         simulation_state: State<'a>,
     }
@@ -10,10 +18,9 @@ pub mod simulation {
             }
         }
 
-        pub fn step(&mut self) {
-            todo!()
-            // self.simulation_state = self.simulation_state.next();
-        }
+        // pub fn step(&mut self) {
+        //     todo!()
+        // }
     }
 
     
@@ -23,10 +30,6 @@ pub mod simulation {
     }
 
     impl<'a> State<'a> {
-        // pub fn next(self) -> Self {
-        //     todo!()
-        // }
-
         /// Add an object to the state.
         pub fn add(mut self, object: &'a impl Object) -> Self {
             self.objects.push(object);
@@ -42,50 +45,93 @@ pub mod simulation {
             self
         }
     }
+}
 
 
+pub mod object {
+    use crate::state::State;
+
+
+    /// A position (meters from origin)
     #[derive(Clone, Copy, Default)]
-    pub struct Position(f32, f32);
+    pub struct Position(f32, f32, f32);
 
-
-    /// An object in the simulation.
-    pub trait Object {
-        /// The position of the object.
-        fn position(&self) -> Position;
-
-        /// Move object. 
-        /// 
-        /// Immovable objects ignore this call (default behavior).
-        fn update_position(&mut self, position: Position) { }
-
-        /// Get next action and update the object's state.
-        fn update(&mut self, state: State) -> Action {
-            Action::DoNothing
+    /// Dimensions of an object: size in meters, weight in kilos
+    #[derive(Clone, Copy, Default)]
+    pub struct Dimensions {
+        /// Size of object in meters
+        size: f32,
+        
+        /// Weight of object in kilos
+        weight: f32,
+    }
+    impl Dimensions {
+        pub fn new(size: f32, weight: f32) -> Self {
+            Self { size, weight }
         }
     }
 
 
-    // pub struct ObjectBehavior {
-    //     pub update: &'static dyn FnMut(State) -> Action,
-    // }
-
-    // impl Default for ObjectBehavior {
-    //     fn default() -> Self {
-    //         Self {
-    //             update: &|_| Action::default(),
-    //         }
-    //     }
-    // }
-
-
+    /// An action an object can take... I guess. Still working on that idea.
     pub enum Action {
         DoNothing,
         // MoveTo(Position),
     }
-
     impl Default for Action {
         fn default() -> Self {
             Self::DoNothing
+        }
+    }
+
+
+    pub enum Duration {
+        Forever,
+        Time(u16),
+    }
+
+
+    /// A simulation object
+    pub trait Object {
+        fn name(&self) -> String;
+
+        /// Position of the object
+        fn position(&self) -> Position;
+
+        /// Dimensions of the object
+        fn dimensions(&self) -> Dimensions;
+
+        /// Move object.
+        /// 
+        /// Immovable objects ignore this call (default behavior).
+        fn move_position(&mut self, position: &Position) { }
+
+        /// Now's the time for you to think, Mr. Rock!
+        /// Will you do nothing forever like every other boring rock,
+        /// or will you actually do something with your life?
+        fn think(&mut self, state: &State) -> (Action, Duration) {
+            (Action::DoNothing, Duration::Forever)
+        }
+    }
+
+
+    pub mod objects {
+        use super::*;
+
+        pub struct Rock {
+            position: Position,
+        }
+        impl Object for Rock {
+            fn name(&self) -> String {
+                "Rock".to_string()
+            }
+
+            fn position(&self) -> Position {
+                self.position
+            }
+
+            fn dimensions(&self) -> Dimensions {
+                Dimensions::new(0.75, 1.0)
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use agent_simulator::simulation::{*, self};
+use agent_simulator::prelude::*;
 
 struct Agent { }
 impl Object for Agent {
+    fn name(&self) -> String {
+        "Agent".to_string()
+    }
+
     fn position(&self) -> Position {
         Position::default()
     }
 
-    fn update(&mut self, state: State) -> Action {
-        Action::DoNothing
+    fn dimensions(&self) -> Dimensions {
+        Dimensions::new(0.1, 1.0)
     }
 }
 
